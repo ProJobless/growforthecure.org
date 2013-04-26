30fa3ecba2a4fe1cd1753c73e3ec063c8d13f803
Stephen Collins <stephen@stephencollins.me>
2013-04-26 13:24:48 -0400

    Created the index.php file.

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Grow for the Cure</title>
+	
+	<meta charset="UTF-8">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/styles.css">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans|Arvo|Francois+One|Yanone+Kaffeesatz:400,300' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body class="homepage">
+
+<?php include 'header_nav.php'; ?>
+
+<div class="style-row inner">
+	<img src="artwork/style-row-960.png" alt="Row of stylized beard icons." width="800px" />
+</div>
+
+
+<div class="tagline inner">
+	<img src="artwork/tagline-razor.png" alt="What could your face do for lung cancer? Img:Big text with razor blade for sahving beards." />
+</div>
+
+<div class="intro-text inner">
+	<p>Welcome to the easiest fundraiser you have signed up for. No more running, walking, climbing stairs or baking. Just put down that razor and relax--Be A Man. Go to Register Now and let your friends bid on a style. Let's find out what type of man you are. <a href="about.php">Read more...</a></p>
+</div>
+
+<div class="right-now inner">
+	<img src="artwork/right-now.png" alt="Right now you could donate or grow." />
+</div>
+
+<div class="donations-where">
+	<div class="inner">
+	
+		<p class="donation-head">Where do your donations go?</p>
+		<p class="donation-desc"><a href="foundation.php"><img src="artwork/foundation_logo.png" width="150px" align="right"></a>All net proceeds will be used by the Bonnie J. Addario Lung Cancer Foundation on the front lines of lung cancer research. Their scientific and medical advisory boards consider only immediate results-oriented projects promising to catalyze progress through early detection, genetic testing, drug discovery and patient-focused outcomes. <a href="http://www.lungcancerfoundation.org" target="_blank">More about the foundation</a>.</p>
+
+		<br clear="all" />
+
+	</div>
+</div>
+
+
+<?php include 'footer.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
