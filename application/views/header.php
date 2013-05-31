65b1d6aa6eccc85963cffe854115b0045eb249be
Stephen Collins <stephen@stephencollins.me>
2013-05-31 11:59:42 -0400

    Viewport Redux

--- application/views/header.php
+++ application/views/header.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/reset.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/styles.css">
 
-	<meta name = "viewport" content = "width = device-width">
+	<meta name="viewport" content="width=970">
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
