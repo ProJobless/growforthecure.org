bb77b7740a593efdcff2eda2618ff1e2dd222d03
Stephen Collins <stephen@stephencollins.me>
2013-05-31 11:57:49 -0400

    viewport again

--- application/views/header.php
+++ application/views/header.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/reset.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/styles.css">
 
-	<meta name="viewport" content="width=960 initial-scale=1.0"> 
+	<meta name = "viewport" content = "width = device-width">
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
