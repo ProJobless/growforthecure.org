f137e4f7288d709db94292b6e8f2d6d5b6e3bd6b
Stephen Collins <stephen@stephencollins.me>
2013-05-31 12:21:30 -0400

    Sponsor page

--- application/logs/log-2013-05-31.php
+++ application/logs/log-2013-05-31.php
@@ -1,3 +1,12 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 ERROR - 2013-05-31 11:51:57 --> 404 Page Not Found --> craig-chicchi
+ERROR - 2013-05-31 12:13:32 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:14:07 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:14:18 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:14:59 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:15:48 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:15:55 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:16:10 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:16:26 --> 404 Page Not Found --> sponsors/artwork
+ERROR - 2013-05-31 12:16:46 --> 404 Page Not Found --> sponsors/artwork
--- application/views/sponsors.php
+++ application/views/sponsors.php
@@ -1,8 +1,17 @@
 <div class="inner">
 
-<?php echo $copy; ?>
+<p class="data-copy"><?php echo $copy; ?> For more information, email us at <a href="mailto:info@growforthecure.org">info@growforthecure.org</a><p>
 
-<br /> <br />
+<h1>Our Sponsors</h1>
+
+
+<div class="sponsor">
+
+<p class="sub-head"><a href="http://www.morphotek.com">Morphotek</a></p>
+<img src="<?php echo base_url(); ?>artwork/sponsors/morphotek.png" width="300px">
+
+<p class="desc">Grow for the Cure gratefully acknowledges the generous support from <a href="http://www.morphotek.com">Morphotek</a>. Morphotek is one of the leading companies in the life science industry that develops novel classes of biological-based products to treat cancer, inflammation and infectious diseases.</p>
+</div>
 
 
 </div>
