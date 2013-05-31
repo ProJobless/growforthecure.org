d36b3a1e3daae4742ecda3f5b419d2c67253f402
Stephen Collins <stephen@stephencollins.me>
2013-05-31 11:57:00 -0400

    Updating viewport

--- application/views/header.php
+++ application/views/header.php
@@ -11,8 +11,8 @@
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/reset.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/styles.css">
 
-	<meta name="viewport" content="width=960"> 
-	
+	<meta name="viewport" content="width=960 initial-scale=1.0"> 
+
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans|Francois+One|Yanone+Kaffeesatz:400,300' rel='stylesheet' type='text/css'>
