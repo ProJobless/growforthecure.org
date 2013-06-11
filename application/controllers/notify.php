847422898d5a7847232bb0af348fb1fac2b2eb72
Stephen Collins <stephen@stephencollins.me>
2013-06-11 17:23:08 -0400

    Sendmail

--- application/controllers/notify.php
+++ application/controllers/notify.php
@@ -13,7 +13,7 @@ class Notify extends CI_Controller {
 		$this->load->library('email');
 
 $config['protocol'] = 'sendmail';
-$config['mailpath'] = '/usr/sbin/sendmail -t -i ';
+$config['mailpath'] = '/usr/sbin/sendmail';
 $config['charset'] = 'iso-8859-1';
 $config['wordwrap'] = TRUE;
 
