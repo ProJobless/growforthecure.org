4add81dd2607066193573293e3c833eb570dd2de
Stephen Collins <stephen@stephencollins.me>
2013-08-07 09:41:27 -0400

    photos

---
+++ application/logs/log-2013-08-07.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-08-07 09:37:42 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:42 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:42 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:42 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:42 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:44 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:44 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:44 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:44 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:44 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:46 --> Severity: Warning  --> unlink(./userphotos/020_(Large).JPG): No such file or directory /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/controllers/deletephoto.php 14
+ERROR - 2013-08-07 09:37:46 --> Severity: Warning  --> unlink(./userphotos/020_(Large)_thumb.JPG): No such file or directory /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/controllers/deletephoto.php 15
+ERROR - 2013-08-07 09:37:46 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/core/Exceptions.php:185) /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/helpers/url_helper.php 540
+ERROR - 2013-08-07 09:37:52 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:52 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:52 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:52 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:37:52 --> 404 Page Not Found --> userphotos
+ERROR - 2013-08-07 09:38:16 --> Severity: Notice  --> Array to string conversion /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/controllers/photos.php 78
+ERROR - 2013-08-07 09:38:16 --> Severity: Warning  --> chown(): Unable to find uid for drummin /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/controllers/photos.php 78
+ERROR - 2013-08-07 09:38:16 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/core/Exceptions.php:185) /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/helpers/url_helper.php 540
+ERROR - 2013-08-07 09:38:33 --> Severity: Notice  --> Undefined variable: data /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/controllers/photos.php 78
+ERROR - 2013-08-07 09:38:33 --> Severity: Warning  --> chown(): Unable to find uid for drummin /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/controllers/photos.php 78
+ERROR - 2013-08-07 09:38:33 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/core/Exceptions.php:185) /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/helpers/url_helper.php 540
+ERROR - 2013-08-07 09:39:40 --> The file you are attempting to upload is larger than the permitted size.
+ERROR - 2013-08-07 09:39:40 --> Severity: Notice  --> Use of undefined constant error - assumed 'error' /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/views/error.php 5
+ERROR - 2013-08-07 09:40:00 --> Severity: Warning  --> chown(): Unable to find uid for drummin /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/application/controllers/photos.php 81
+ERROR - 2013-08-07 09:40:00 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/core/Exceptions.php:185) /Users/scollins/Dropbox/Projects/Current_Sites/growforthecure.org/system/helpers/url_helper.php 540
